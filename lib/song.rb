52902d6db82685ab1859db86617fbade70f743b9
John Troutman <jtroutmanman@gmail.com>
2020-10-04 18:34:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
   def self.new_by_filename(file)
     file = file.split(" - ")
     song = self.new(file[1])
-    song.artist = song.artist_name=(file[0])
+    song.artist_name=(file[0])
     song
   end
   
